ae9a785371007269994cf6112907ce1b6f5c961f
Destuur <155296556+Destuur@users.noreply.github.com>
2025-06-03 11:59:50 +0200

    Create repo

---
+++ RazorClassLibrary/Layout/NavMenu.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace RazorClassLibrary.Layout
+{
+	public partial class NavMenu
+	{
+
+	}
+}
---
+++ WpfBlazor.MudTemplate/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MudBlazor.Services;
+using System.Windows;
+
+namespace WpfBlazor.MudTemplate
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			var serviceCollection = new ServiceCollection();
+			serviceCollection.AddWpfBlazorWebView();
+			serviceCollection.AddMudServices();
+#if DEBUG
+			serviceCollection.AddBlazorWebViewDeveloperTools();
+#endif
+
+			var serviceProvider = serviceCollection.BuildServiceProvider();
+			Resources.Add("services", serviceProvider);
+
+			InitializeComponent();
+		}
+	}
+}
